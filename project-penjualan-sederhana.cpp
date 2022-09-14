89bc3ebb2128cef3bdc5f9337eca2602e59a791d
willzyx-hub <86699027+willzyx-hub@users.noreply.github.com>
2022-09-14 20:15:52 +0700

    Update project-penjualan-sederhana.cpp

--- project-penjualan-sederhana.cpp
+++ project-penjualan-sederhana.cpp
@@ -1,22 +1,58 @@
 #include <iostream>
-#include <stdio.h>
-#include <time.h>
-#include <windows.h>
-
-// using namespace std
+// using namespace std;
 
 int main () {
+    int harga, jml_barang, total, pilihan;
+    std::string item;
+    std::cout << "Item yang Tersedia : " << std::endl;
+    std::cout << "1. Keyboard : Rp. 165.000" << std::endl;
+    std::cout << "2. Mouse : Rp. 75.000" << std::endl;
+    std::cout << "3. Flashdisk 16GB : Rp. 82.000" << std::endl;
+    std::cout << "4. Keluar Aplikasi" << std::endl;
+    do {
+        std::cout << "Input Kode ";
+        std::cin >> pilihan;
+    switch(pilihan) {
+        case 1 :
+            item = "Keyboard";
+            harga = 165000;
+            std::cout << "Jumlah Item yang akan Dibeli : ";
+            std::cin >> jml_barang;
+            total += harga * jml_barang;
+            std::cout << jml_barang << " " << item;
+            std::cout << " = Rp. "<<harga*jml_barang << std::endl;
+            break;
 
-char nama[30],pembeli[30];
-int harga, jumlah, total, jumtotal, jumbayar, kembalian ;
-std::string input, kode;
+        case 2 :
+        item = "Mouse";
+        harga = 75000;
+        std::cout << "Jumlah Item yang akan Dibeli : ";
+        std::cin >> jml_barang;
+        total += harga * jml_barang;
+        std::cout << jml_barang << " " << item;
+        std::cout << " = Rp. "<<harga*jml_barang << std::endl;
+        break;
 
-std::cout << "SELAMAT DATANG DI TOKO KOMPUTER X" << std::endl;
-std::cout << "=================================" << std::endl;
-std::cout << "NAMA PEMBELI      : " << std::endl;
-std::cin >> pembeli;
-std::cout << "Pilihan Yang Tersedia" << std::endl;
-std::cout << "1. Keyboard Rp 175.000" << std::endl;
-std::cout << "2. Mouse Rp 47.500" << std::endl;
-std::cout << "3. Flashdisk 16GB Rp 78.000" << std::endl;
-}
+        case 3 :
+        item = "Flashdisk 16GB";
+        harga = 82000;
+        std::cout << "Jumlah Item yang akan Dibeli : ";
+        std::cin >> jml_barang;
+        total += harga * jml_barang;
+        std::cout << jml_barang << " " << item;
+        std::cout << " = Rp. "<<harga*jml_barang << std::endl;
+        break;
+
+        case 4 :
+            std::cout << "Total Belanja : Rp. " << total << std::endl;
+            pilihan = 4;
+            break;
+        default :
+            std::cout << "FAILED : Input tidak valid." << std::endl;
+            break;
+        }
+    } while(pilihan != 4);
+    system("pause > 0"); // This is not recommended to use because This Function is not portable and only work for Windows Operating System
+    std::cout << "Terimakasih Telah berbelanja disini";
+    
+} 
