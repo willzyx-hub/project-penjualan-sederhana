85982397f9d9d72ed278cd962a487b231e85cdbd
willzyx-hub <86699027+willzyx-hub@users.noreply.github.com>
2022-09-14 01:02:27 -0700

    First Commit

---
+++ project-penjualan-sederhana.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+#include <time.h>
+#include <windows.h>
+
+// using namespace std
+
+int main () {
+
+char nama[30],pembeli[30];
+int harga, jumlah, total, jumtotal, jumbayar, kembalian ;
+std::string input, kode;
+
+std::cout << "SELAMAT DATANG DI TOKO KOMPUTER X" << std::endl;
+std::cout << "=================================" << std::endl;
+std::cout << "NAMA PEMBELI      : " << std::endl;
+std::cin >> pembeli;
+std::cout << "Pilihan Yang Tersedia" << std::endl;
+std::cout << "1. Keyboard Rp 175.000" << std::endl;
+std::cout << "2. Mouse Rp 47.500" << std::endl;
+std::cout << "3. Flashdisk 16GB Rp 78.000" << std::endl;
+}
